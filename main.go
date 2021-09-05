4df670473b8066b9f1341494b8154686efa13ec7
haerong22 <haerong22@naver.com>
2021-09-05 18:11:00 +0900

    rendering template

--- main.go
+++ main.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"text/template"
 
 	"github.com/haerong22/bobbycoin/blockchain"
 )
 
-func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Fourth Block")
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
 
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data : %s\n", block.Data)
-		fmt.Printf("Hash : %s\n", block.Hash)
-		fmt.Printf("PrevHash : %s\n", block.PrevHash)
-	}
+var templates *template.Template
+
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	templates.ExecuteTemplate(rw, "home", data)
+}
+
+func main() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+
+	http.HandleFunc("/", home)
+
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
