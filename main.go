79602f57349163186f8a0a25e57cbf9dfd0bafd2
haerong22 <haerong22@naver.com>
2021-09-13 23:05:21 +0900

    go routine, buffered(unbuffered) channel

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
-	"github.com/haerong22/bobbycoin/cli"
-	"github.com/haerong22/bobbycoin/db"
+	"fmt"
+	"time"
 )
 
+func receive(c <-chan int) {
+	for {
+		time.Sleep(10 * time.Second)
+		a, ok := <-c
+		if !ok {
+			fmt.Println("done")
+			break
+		}
+		fmt.Printf("|| receive %d ||\n", a)
+	}
+}
+
+func send(c chan<- int) {
+	for i := range [10]int{} {
+		fmt.Printf(">> sending %d <<\n", i)
+		c <- i
+		fmt.Printf(">> sent %d <<\n", i)
+	}
+	close(c)
+}
+
 func main() {
-	defer db.Close()
-	cli.Start()
+	// defer db.Close()
+	// cli.Start()
+	c := make(chan int, 10)
+	go send(c)
+	receive(c)
 }
