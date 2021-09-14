b84b05df692fff0e4f541ec99551916d9215598b
haerong22 <haerong22@naver.com>
2021-09-15 00:21:24 +0900

    connect peer to peer

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,33 +1,30 @@
 package p2p
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 	"github.com/haerong22/bobbycoin/utils"
 )
 
-var conns []*websocket.Conn
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
-	upgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
-	}
-	conn, err := upgrader.Upgrade(rw, r, nil)
-	conns = append(conns, conn)
-	utils.HandleErr(err)
+	openPort := r.URL.Query().Get("openPort")
+	ip := utils.Splitter(r.RemoteAddr, ":", 0)
 
-	for {
-		_, p, err := conn.ReadMessage()
-		if err != nil {
-			conn.Close()
-			break
-		}
-		for _, aConn := range conns {
-			if aConn != conn {
-				utils.HandleErr(aConn.WriteMessage(websocket.TextMessage, p))
-			}
-		}
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return openPort != "" && ip != ""
 	}
+
+	conn, err := upgrader.Upgrade(rw, r, nil)
+	utils.HandleErr(err)
+	initPeer(conn, ip, openPort)
+}
+
+func AddPeer(address, port, openPort string) {
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
+	utils.HandleErr(err)
+	initPeer(conn, address, port)
 }
