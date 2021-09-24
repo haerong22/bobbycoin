5926b42d6a36611421a567e273a638b13e6034d0
haerong22 <haerong22@naver.com>
2021-09-25 00:50:42 +0900

    wallet test : sign, verify test

---
+++ wallet/wallet_test.go
@@ -0,0 +1,56 @@
+package wallet
+
+import (
+	"crypto/x509"
+	"encoding/hex"
+	"testing"
+)
+
+const (
+	testKey          string = "30770201010420e043ce091f7c0f7c4fe832b742b3618d33aa89bf8e181a39de1c5668b4326918a00a06082a8648ce3d030107a14403420004f37635030b40fac5dd049c2330e2727252cb51a23418c806b5e13f5cb6c8cd318e17c455a30f16531d66ccead000a45afca5014fd6ade9ee75e31efd7491fe75"
+	testPayload      string = "00f4cda7b12269032a9cbd30178aa593b522989319c92fb4a1ebdf256c94f2e9"
+	testWrongPayload string = "04f4cda7b12269032a9cbd30178aa593b522989319c92fb4a1ebdf256c94f2e9"
+	testSig          string = "fc103e9de2715f4af1ce7193bb6d899b6f03baa2a9efa39f5793320736fea8fb7b894fd34ce3b1de99f571d8446ef76050e58a341f9ba9ecb646939de861c192"
+)
+
+func makeTestWallet() *wallet {
+	w := &wallet{}
+	b, _ := hex.DecodeString(testKey)
+	key, _ := x509.ParseECPrivateKey(b)
+	w.privateKey = key
+	w.Address = aFromK(key)
+	return w
+}
+
+func TestSign(t *testing.T) {
+	s := Sign(testPayload, makeTestWallet())
+	_, err := hex.DecodeString(s)
+	if err != nil {
+		t.Errorf("Sign() should return a hex encoded string, got %s.", s)
+	}
+}
+
+func TestVerify(t *testing.T) {
+	type test struct {
+		input string
+		ok    bool
+	}
+	tests := []test{
+		{testPayload, true},
+		{testWrongPayload, false},
+	}
+	for _, tc := range tests {
+		w := makeTestWallet()
+		ok := Verify(testSig, tc.input, w.Address)
+		if ok != tc.ok {
+			t.Error("Verify() could not verify testSignature and testPayload.")
+		}
+	}
+}
+
+func TestRestoreBigInts(t *testing.T) {
+	_, _, err := restoreBigInts("xx")
+	if err == nil {
+		t.Error("RestoreBigInts should return error when payload is not hex.")
+	}
+}
